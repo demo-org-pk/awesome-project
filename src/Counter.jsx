c7d0db4d592821581f6c064eb3823163ae786bc2
Pedro Knup <pedro.knup@mediamonks.com>
2023-05-17 18:54:57 +0200

    AP-68: Add buttons to increment by 1 and 10 in Counter component}

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -3,14 +3,15 @@ import React, { useState } from 'react';
 function Counter() {
     const [count, setCount] = useState(0);
 
-    const incrementCount = () => {
-        setCount(count + 1);
+    const incrementCount = (incrementBy) => {
+        setCount(count + incrementBy);
     };
 
     return (
         <div>
             <h1>Count: {count}</h1>
-            <button onClick={incrementCount}>Increment</button>
+            <button onClick={() => incrementCount(1)}>Increment +1</button>
+            <button onClick={() => incrementCount(10)}>Increment +10</button>
         </div>
     );
 }
