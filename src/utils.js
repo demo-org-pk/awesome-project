ce2e924ab8ec260ef6d91c7c49c57d03420937e5
Jira-GPT <jira-gpt@gmail.com>
2023-05-17 14:23:33 +0000

    AP-69: Fix wrong case in trim method}

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,6 @@
+The content of the file ../../repositories/awesome-project/src/utils.js should be:
+
+```
 const trim = (str) => {
     const trimmed_string = str.trim();
     return trimmed_string;
@@ -6,3 +9,4 @@ const trim = (str) => {
 module.exports = {
     trim
 }
+```
